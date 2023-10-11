bc9885870f16616cd5580b2fed8a914253120706
betarunner <143918699+betarunner@users.noreply.github.com>
2023-10-10 18:06:50 -0700

    home complete

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Add a route for the Home Component
+  { path: 'home', component: HomeComponent },
+  // Set a default route (redirect to Home Component)
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-auditions',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+
+export class HomeComponent {
+  scrollToSection(sectionId: string): void {
+    // remove active class from all tabs
+    document.querySelectorAll('.tab-option').forEach(tab => {
+      tab.classList.remove('active');
+    });
+  
+    // add active class to the clicked tab
+    document.querySelector(`.tab-option[click*="${sectionId}"]`)?.classList.add('active');
+
+    const element = document.querySelector('#' + sectionId);
+    if (element) {
+      const offsetTop = element.getBoundingClientRect().top + window.scrollY - 200; // Adjust 200 to the desired offset
+      window.scrollTo({ top: offsetTop, behavior: 'smooth' });
+    }
 }
+  
+}
+
