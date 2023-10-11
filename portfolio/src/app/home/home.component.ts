e9cbfaee0f92f67b46b070b8d468d8afd7a2797b
betarunner <143918699+betarunner@users.noreply.github.com>
2023-10-10 22:48:08 -0700

    completed all portfolio

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 
 
 export class HomeComponent {
+
+  currentPhotoIndex = 0;
+  photos = [
+    'assets/tritone project/1.png',
+    'assets/tritone project/2.png',
+    'assets/tritone project/3.png',
+    'assets/tritone project/4.png',
+    'assets/tritone project/5.png',
+  ];
+
   scrollToSection(sectionId: string): void {
     // remove active class from all tabs
     document.querySelectorAll('.tab-option').forEach(tab => {
@@ -19,10 +29,31 @@ export class HomeComponent {
 
     const element = document.querySelector('#' + sectionId);
     if (element) {
-      const offsetTop = element.getBoundingClientRect().top + window.scrollY - 200; // Adjust 200 to the desired offset
+      const offsetTop = element.getBoundingClientRect().top + window.scrollY - 0;
       window.scrollTo({ top: offsetTop, behavior: 'smooth' });
     }
-}
+  }
+
+  private updatePhotoSrc(): void {
+    const imgElement = document.querySelector('#photo') as HTMLImageElement;
+    if (imgElement) {
+      imgElement.src = this.photos[this.currentPhotoIndex];
+    }
+  }
+
+  ngOnInit(): void {
+    this.updatePhotoSrc();
+  }
+
+  getNextPhoto(): void {
+    this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.photos.length;
+    this.updatePhotoSrc();
+  }
+
+  getPrevPhoto(): void {
+    this.currentPhotoIndex = (this.currentPhotoIndex - 1 + this.photos.length) % this.photos.length;
+    this.updatePhotoSrc();
+  }
   
 }
 
